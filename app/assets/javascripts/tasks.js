f6b2c6f303f0cfaecb9a5691a6168db25f23f2c6
Kyle Gundry <Gundr1kt@gmail.com>
2017-04-24 21:07:45 -0500

    Add strikethrough when click on Task update to array of hashes

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -3,21 +3,37 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var app = new Vue({
     el: '#app',
     data: {
-      tasks: ["Wake Up!", "Eat Food!", "Brush Teeth!"],
+      tasks: [
+      {name: "Wake Up!", complete: false}, 
+      {name: "Eat Food!", complete: false},
+      {name: "Brush Teeth!", complete: false}
+      ],
       newTask: ""
     },
     methods: {
       addTask: function() {
         //console.log("I'm gonna add the review now...", this.newReview);
         if (this.newTask !== "") {
-          this.tasks.push(this.newTask);
+          this.tasks.push({name: this.newTask, complete: false});
           this.newTask = "";
         }
       },
       deleteTask: function(inputTask) {
         var index = this.tasks.indexOf(inputTask);
         this.tasks.splice(index, 1);
+      },
+      toggle: function(inputTask) {
+        inputTask.complete = !inputTask.complete;
       }
+      /* isPostive: function(inputReview) {
+        return (inputReview.indexOf("bad") === -1) OR
+        if if (inputReview.indexOf("bad") === -1) {
+          return = false;
+        else
+          return = true;
+        end
+        }
+      }*/
     }
   });
 });
